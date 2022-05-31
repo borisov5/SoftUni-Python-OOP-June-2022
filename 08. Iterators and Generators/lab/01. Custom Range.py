9ede788dbde630c1c0d86161ee2d71350b0ae0ea
Ivelin Borisov <90949939+borisov5@users.noreply.github.com>
2022-05-31 16:07:48 +0300

    Add files via upload

---
+++ 08. Iterators and Generators/lab/01. Custom Range.py
@@ -0,0 +1,19 @@
+class custom_range:
+    def __init__(self, start, end):
+        self.i = start
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.i <= self.end:
+            current = self.i
+            self.i += 1
+            return current
+        else:
+            raise StopIteration()
+
+on_to_ten = custom_range(1, 10)
+for num in on_to_ten:
+    print(num)
