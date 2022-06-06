bff45de7a5279950069f8310805d11e9741afead
Ivelin Borisov <90949939+borisov5@users.noreply.github.com>
2022-06-06 17:53:45 +0300

    Add files via upload

---
+++ 09. Decorators/Lab/02. Vowel Filter.py
@@ -0,0 +1,12 @@
+def vowel_filter(function):
+    def wrapper():
+        letters = function()
+        return [letter for letter in letters if letter.lower() in "aeuoi"]
+    return wrapper
+
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+print(get_letters())
