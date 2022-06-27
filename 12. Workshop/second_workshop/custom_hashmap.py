43d0ce5abe1fabf43a28d944dd7ef7763cfbf6b3
Ivelin Borisov <90949939+borisov5@users.noreply.github.com>
2022-06-27 21:33:08 +0300

    Update custom_hashmap.py

--- 12. Workshop/second_workshop/custom_hashmap.py
+++ 12. Workshop/second_workshop/custom_hashmap.py
@@ -4,6 +4,13 @@ class HashTable:
         self.__keys = [None] * self.max_capacity
         self.__values = [None] * self.max_capacity
 
+    def __getitem__(self, key):
+        try:
+            index = self.__keys.index(key)
+            return self.__values[index]
+        except ValueError:
+            raise KeyError(key)
+
     def __setitem__(self, key, value):
         if key in self.__keys:
             index = self.__keys.index(key)
@@ -31,6 +38,9 @@ class HashTable:
     def size(self):
         return len([el for el in self.__keys if el is not None])
 
+    def add(self, key, value):
+        self[key] = value
+
     def __resize(self):
         self.__keys = self.__keys + [None] * self.max_capacity
         self.__values = self.__values + [None] * self.max_capacity
@@ -40,6 +50,17 @@ class HashTable:
         keys_values = [f"{self.__keys[index]}: {self.__values[index]}"
                        for index in range(len(self.__keys))
                        if self.__keys[index] is not None]
+        return "{" + ", ".join(keys_values) + "}"
+
+    def get(self, key, default=None):
+        try:
+            index = self.__keys.index(key)
+            return self.__values[index]
+        except ValueError:
+            return default
+
+    def __len__(self):
+        return self.max_capacity
 
 
 table = HashTable()
@@ -49,10 +70,8 @@ table["age"] = 25
 table["is_pet_owner"] = True
 table["weight"] = 100
 table["some"] = "Test"
-table["name"] = "Ines"
-a = 5
 
 print(table)
-# print(table.get("name"))
-# print(table["age"])
-# print(len(table))
+print(table.get("name", "asd"))
+print(table["age"])
+print(len(table))
