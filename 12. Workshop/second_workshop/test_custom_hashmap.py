18ae33f29a707b5d07449fc2a3839b4231b7d6b4
Ivelin Borisov <90949939+borisov5@users.noreply.github.com>
2022-06-27 21:33:41 +0300

    Update test_custom_hashmap.py

--- 12. Workshop/second_workshop/test_custom_hashmap.py
+++ 12. Workshop/second_workshop/test_custom_hashmap.py
@@ -1,3 +1,39 @@
 # Coll
 # Overflow
 # Same key -> should not be duplicated
+# Test __str__ logic
+
+
+from unittest import main, TestCase
+
+from custom_hashmap import HashTable
+
+
+class TestHashTable(TestCase):
+    def test_init(self):
+        table = HashTable()
+        self.assertEqual([None, None, None, None], table._HashTable__keys)
+        self.assertEqual([None, None, None, None], table._HashTable__values)
+        self.assertEqual(4, table.max_capacity)
+
+    def test_get_item_dunder(self):
+        table = HashTable()
+        table["name"] = "Test"
+        table["age"] = 0
+        result = table["name"]
+        self.assertEqual("Test", result)
+
+    def test_get_item_dunder_non_existing_key_raises(self):
+        table = HashTable()
+        table["name"] = "Test"
+        table["age"] = 0
+        with self.assertRaises(KeyError) as ex:
+            # Non existing key
+            table["asd"]
+        self.assertEqual("asd", str(ex.exception.args[0]))
+
+
+
+
+if __name__ == "__main__":
+    main()
